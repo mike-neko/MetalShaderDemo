a952e5449dc8e095ed7413f164c285cf1e3d6adc
Makoto Ikeda <mike.app.info@gmail.com>
2016-08-07 00:38:27 +0900

    初期コミット

---
+++ MetalShaderDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MetalShaderDemo
+//
+//  Created by M.Ike on 2016/08/07.
+//  Copyright (c) 2016年 M.Ike. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
